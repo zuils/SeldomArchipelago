3b090e9449f896535896ad4de745768bb724caec
desperandos101 <35909577+desperandos101@users.noreply.github.com>
2025-11-20 13:06:04 -0500

    Reorder options

--- Config/Config.cs
+++ Config/Config.cs
@@ -30,12 +30,12 @@ namespace SeldomArchipelago.Config
 
         [Header("Miscellaneous")]
 
-        [Label("Debug Mode")]
-        [DefaultValue(false)]
-        public bool debug;
-
         [Label("Receive Hardmode As Item")]
         [DefaultValue(false)]
         public bool hardmodeAsItem;
+
+        [Label("Debug Mode")]
+        [DefaultValue(false)]
+        public bool debug;
     }
 }
