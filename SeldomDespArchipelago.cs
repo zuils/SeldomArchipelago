31b562e79b9590799c04cfeaa4144f7f313a18a4
desperandos101 <35909577+desperandos101@users.noreply.github.com>
2026-03-07 15:36:45 -0500

    Add calamity version + AP concurrency checking

--- SeldomDespArchipelago.cs
+++ SeldomDespArchipelago.cs
@@ -81,6 +81,10 @@ namespace SeldomDespArchipelago
         public override void Load()
         {
             var archipelagoSystem = ModContent.GetInstance<ArchipelagoSystem>();
+            if (ModLoader.HasMod("SeldomArchipelago") || ModLoader.HasMod("SeldomArchipelagoExtended") || ModLoader.HasMod("SpikersArchipelago"))
+            {
+                throw new Exception("Multiple Archipelago mods detected! Make sure only one is enabled at a time.");
+            }
 
             // Begin cursed IL editing
 
@@ -695,6 +699,11 @@ namespace SeldomDespArchipelago
 
             if (!ModLoader.HasMod("CalamityMod")) return;
             var calamity = ModLoader.GetMod("CalamityMod");
+
+            if (!calamity.Version.Equals(new Version(2, 0, 6, 2)))
+            {
+                throw new Exception("Incompatible Calamity version. This is most likely because you are using the workshop version of Calamity.\nPlease reload with Calamity 2.0.6.2. For information on how to do that, see the pins in the Archipelago discord's #terraria channel");
+            }
 
             var calamityAssembly = calamity.GetType().Assembly;
             foreach (var type in calamityAssembly.GetTypes()) switch (type.Name)
